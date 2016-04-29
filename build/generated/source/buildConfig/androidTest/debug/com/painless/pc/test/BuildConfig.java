155c184c13318e0c486de3d23d0744b2a772782a
krknight <keith.knight@rockwellcollins.com>
2016-04-29 09:57:58 -0700

    first walk 042916

---
+++ build/generated/source/buildConfig/androidTest/debug/com/painless/pc/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.painless.pc.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.painless.pc.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 160604;
+  public static final String VERSION_NAME = "6.0.4";
+}
